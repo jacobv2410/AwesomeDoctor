cfe9704204e41c3c52062728e22bbfdd0151ad28
Jacobv2410 <jacobv2410@gmail.com>
2018-03-14 15:22:13 -0700

    final upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -64,8 +64,8 @@ $(document).ready(function() {
     var coordinatesValue = [];
     $('#search').on('click', function() {
         
-        $('#doctorData').show()
-        $('#doctorData').empty();
+        $('#doctorData').show();
+        
         coordinatesValue = [];
 
         specialty = $('#specialty').val();
@@ -83,7 +83,7 @@ $(document).ready(function() {
 
                 coordinatesValue.push(latitude, longtitude);
 
-            }           
+            }
 
             betterDoctor(specialty, coordinatesValue[0], coordinatesValue[1], radius);           
 
@@ -102,6 +102,8 @@ $(document).ready(function() {
             url: resource_url,
             method: 'GET'
         }).then(function(resp) {
+            // debugger
+            // $('.body-item').remove();
             console.log(resp);
 
             doctorArray = resp.data;
@@ -120,11 +122,6 @@ $(document).ready(function() {
                 // this is an array
                 var specialties = convertArrayObjectToString(doctorArray[i].specialties);                
                 var li = $("<li class='item' data-index='" + i + "'><div class='collapsible-header title-header'>" + firstName + " " + lastName + ", " + title + " - Specialities: " + specialties + "</div><div class='collapsible-body body-item'><div class='row'><div class='col m2 s12'><img class='responsive-img avatar' src='" + image + "'></div><div class='col m10 s12 bio'></div></div><div id='map'></div></div>");
-
-                
-
-
-
                 $("#doctorData").append(li);
             }
 
@@ -135,21 +132,21 @@ $(document).ready(function() {
     };
 
     $(document).on("click", ".item", function() {
+        //$(".item").empty();
         var index = $(this).attr("data-index");
         var content = $(this).find(".bio");
 
         var address = getOfficeAddress(doctorArray[index].practices[0]);        
         var numbers = displayContactNumbers(doctorArray[index].practices[0].phones);
-
         console.log("address: " + address);
         console.log("numbers: " + numbers);
         
         var bio = "<p id='intro'>" + doctorArray[index].profile.bio + "</p>";
-        var contacts = "<div id='contact-info'>";        
+        var contacts = "<div id='contact-info'>";
+        console.log(bio);
         contacts += address + "<br>" + numbers;
         contacts += "</div>";
-
-
+        content.empty();
         content.append(bio, contacts);        
         
         // add google map in here
